6c531a1f5a2948cafafde6d3306da65340cefeb3
nhp2000199 <nhp200199@gmail.com>
2024-03-31 14:58:12 +0700

    fix bug Couple Days not correct

--- app/src/main/java/com/example/weareloversbackup/data/constant/general.kt
+++ app/src/main/java/com/example/weareloversbackup/data/constant/general.kt
@@ -1,5 +1,5 @@
 package com.example.weareloversbackup.data.constant
 
-const val DEFAULT_DATE_FORMAT = ""
+const val DEFAULT_DATE_FORMAT = "dd/MM/yyyy"
 
 const val DEFAULT_IMAGE_PATH = "android.resource://com.example.weareloversbackup/drawable/couple"
